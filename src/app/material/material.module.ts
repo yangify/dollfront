27983f6a825a4de54d09f6fe23eb47b7cff2f4e6
hongyang <hong.yang.code@gmail.com>
2021-05-03 10:30:46 +0800

    added bare header

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 const material = [
-  MatToolbarModule
+  MatToolbarModule,
+  MatButtonModule,
+  MatIconModule
 ];
 
 @NgModule({
