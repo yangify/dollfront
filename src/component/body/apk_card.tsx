ea5605018cf28fd15640c22d1f452be676900a83
hongyang <hong.yang.code@gmail.com>
2021-05-11 17:31:34 +0800

    updated apk card styling

--- src/component/body/apk_card.tsx
+++ src/component/body/apk_card.tsx
@@ -2,32 +2,50 @@ export const ApkCard = () => {
     return (
         <div className="card h-100">
             <div className="card-header">
-                Featured
+                APK
             </div>
             <div className="card-body overflow-auto">
                 <div className="list-group list-group-flush">
-                    <a href="/" className="list-group-item list-group-item-action active" aria-current="true">
-                        The current link item
-                    </a>
-                    <a href="/" className="list-group-item list-group-item-action">A second link item</a>
-                    <a href="/" className="list-group-item list-group-item-action">A third link item</a>
-                    <a href="/" className="list-group-item list-group-item-action">A fourth link item</a>
-                    <a href="/" className="list-group-item list-group-item-action">A second link item</a>
-                    <a href="/" className="list-group-item list-group-item-action">A third link item</a>
-                    <a href="/" className="list-group-item list-group-item-action">A fourth link item</a>
-                    <a href="/" className="list-group-item list-group-item-action">A second link item</a>
-                    <a href="/" className="list-group-item list-group-item-action">A third link item</a>
-                    <a href="/" className="list-group-item list-group-item-action">A fourth link item</a>
-                    <a href="/" className="list-group-item list-group-item-action">A second link item</a>
-                    <a href="/" className="list-group-item list-group-item-action">A third link item</a>
-                    <a href="/" className="list-group-item list-group-item-action">A fourth link item</a>
-                    <a href="/" className="list-group-item list-group-item-action">A second link item</a>
-                    <a href="/" className="list-group-item list-group-item-action">A third link item</a>
-                    <a href="/" className="list-group-item list-group-item-action">A fourth link item</a>
-                    <a href="/" className="list-group-item list-group-item-action">A second link item</a>
-                    <a href="/" className="list-group-item list-group-item-action">A third link item</a>
-                    <a href="/" className="list-group-item list-group-item-action">A fourth link item</a>
-                    <a href="/" className="list-group-item list-group-item-action disabled">A disabled link item</a>
+                    <button className="btn list-group-item list-group-item-action list-group-item-primary d-flex justify-content-between align-items-start">
+                        <i className="bi bi-archive-fill mt-3 me-2"/>
+                        <div className="ms-2 me-auto">
+                            <div className="fw-bold">Subheading</div>
+                            Cras justo odio
+                        </div>
+                        <span className="badge bg-success rounded-pill mt-3 p-2 ">
+                            <i className="bi bi-check-lg"/>
+                        </span>
+                    </button>
+                    <li className="btn list-group-item list-group-item-action d-flex justify-content-between align-items-start">
+                        <i className="bi bi-archive mt-3 me-2"/>
+                        <div className="ms-2 me-auto">
+                            <div className="fw-bold">Subheading</div>
+                            Cras justo odio
+                        </div>
+                        <span className="badge bg-warning rounded-pill mt-3 p-2 ">
+                            <i className="bi bi-three-dots"/>
+                        </span>
+                    </li>
+                    <li className="btn list-group-item list-group-item-action d-flex justify-content-between align-items-start">
+                        <i className="bi bi-archive mt-3 me-2"/>
+                        <div className="ms-2 me-auto">
+                            <div className="fw-bold">Subheading</div>
+                            Cras justo odio
+                        </div>
+                        <span className="badge bg-success rounded-pill mt-3 p-2 ">
+                            <i className="bi bi-check-lg"/>
+                        </span>
+                    </li>
+                    <li className="btn list-group-item list-group-item-action d-flex justify-content-between align-items-start">
+                        <i className="bi bi-archive mt-3 me-2"/>
+                        <div className="ms-2 me-auto">
+                            <div className="fw-bold">Subheading</div>
+                            Cras justo odio
+                        </div>
+                        <span className="badge bg-danger rounded-pill mt-3 p-2 ">
+                            <i className="bi bi-x-lg"/>
+                        </span>
+                    </li>
                 </div>
             </div>
         </div>
--- src/component/body/body.tsx
+++ src/component/body/body.tsx
@@ -3,8 +3,8 @@ import {LinkPanel} from "./link_panel";
 
 export const Body = () => {
     return (
-        <div className="container px-1 h-75">
-            <div className="row g-2 h-100" >
+        <div className="container-fluid px-4 py-2 h-75">
+            <div className="row g-4 h-100" >
                 <div className="col-lg-4 h-100" >
                     <ApkCard />
                 </div>
