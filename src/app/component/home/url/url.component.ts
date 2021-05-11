9b72b3226722d0d37d5c13cc915d484a6828f6cc
hongyang <hong.yang.code@gmail.com>
2021-05-11 10:44:35 +0800

    updated card to display data from api

--- src/app/component/home/url/url.component.ts
+++ src/app/component/home/url/url.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import axios from 'axios';
+import {environment} from '../../../../environments/environment';
 
 @Component({
   selector: 'app-url',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UrlComponent implements OnInit {
 
+  data: object = {
+    decompiler1: [{ path: 'path', link: 'link'}, { path: 'path', link: 'link'}],
+    decompiler2: [{ path: 'path', link: 'link'}, { path: 'path', link: 'link'}]
+  };
+
   constructor() { }
 
   ngOnInit(): void {
+    // axios
+    //   .get(environment.backendUrl + environment.linkEndpoint)
+    //   .then(r => this.data = r.data );
   }
 
 }
