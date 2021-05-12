d6e92c76c22085d38753d730ffcc3ef8c1626c99
hongyang <hong.yang.code@gmail.com>
2021-05-12 15:42:19 +0800

    updated menu

--- src/component/header/menu.jsx
+++ src/component/header/menu.jsx
@@ -3,8 +3,7 @@ export const Menu = () => {
         <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
             <li><a href="/" className="nav-link px-2 text-secondary">Home</a></li>
             <li><a href="/" className="nav-link px-2 text-white">Upload</a></li>
-            <li><a href="/" className="nav-link px-2 text-white">Progress</a></li>
-            <li><a href="/" className="nav-link px-2 text-white">ElasticSearch</a></li>
+            <li><a href="/" className="nav-link px-2 text-white">Explore</a></li>
         </ul>
     );
 }
