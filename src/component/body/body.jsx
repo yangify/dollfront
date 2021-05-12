011a26f5072a9f860f73d9b9b8ea3f9aa3fe0d2f
hongyang <hong.yang.code@gmail.com>
2021-05-12 16:56:24 +0800

    refactored home directory

--- src/component/body/body.jsx
+++ src/component/body/body.jsx
@@ -1,17 +1,9 @@
-import { ApkCard } from "./apk/apk_card";
-import { LinkCard } from "./link/link_card";
+import { Home } from "./home/home";
 
 export const Body = () => {
     return (
         <div className="container-fluid h-75 p-4">
-            <div className="row h-100" >
-                <div className="col-xl-5 h-100 mb-4" >
-                    <ApkCard />
-                </div>
-                <div className="col-xl-7 h-100 mb-4">
-                    <LinkCard />
-                </div>
-            </div>
+            <Home />
         </div>
     );
 }
