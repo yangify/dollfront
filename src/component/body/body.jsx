080e5bceba9a04e1c818a1f819de67e56c28ea1c
hongyang <hong.yang.code@gmail.com>
2021-05-12 16:02:36 +0800

    added margin for columns

--- src/component/body/body.jsx
+++ src/component/body/body.jsx
@@ -5,10 +5,10 @@ export const Body = () => {
     return (
         <div className="container-fluid h-75 p-4">
             <div className="row h-100" >
-                <div className="col-xl-5 h-100" >
+                <div className="col-xl-5 h-100 mb-4" >
                     <ApkCard />
                 </div>
-                <div className="col-xl-7 h-100">
+                <div className="col-xl-7 h-100 mb-4">
                     <LinkCard />
                 </div>
             </div>
