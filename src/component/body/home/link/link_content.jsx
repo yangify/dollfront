9553af33c42484c0e6691ce9c62709745c913037
hongyang <hong.yang.code@gmail.com>
2021-05-18 15:59:41 +0800

    code clean

--- src/component/body/home/link/link_content.jsx
+++ src/component/body/home/link/link_content.jsx
@@ -1,12 +1,15 @@
 import { LinkList } from "./link_list";
 
 export const LinkContent = ({ content }) => {
+
+    const dormant = "tab-pane fade p-2";
+    const active = dormant + " show active";
+
     return (
         <div className="tab-content" id="nav-tabContent">
             {
                 Object.keys(content).map((tool, index) => (
-                    <div className={ "tab-pane fade p-2 " + (index === 0 ? 'show active' : '') }
-                         key={index} id={tool} role="tabpanel">
+                    <div className={ index === 0 ? active : dormant } key={index} id={tool} role="tabpanel">
                         <LinkList links={content[tool]}/>
                     </div>
                 ))
--- src/component/body/home/link/link_nav.jsx
+++ src/component/body/home/link/link_nav.jsx
@@ -1,11 +1,14 @@
 export const LinkNav = ({ tools }) => {
+
+    const dormant = "nav-link";
+    const active = dormant + " active";
+
     return (
         <nav>
             <div className="nav nav-pills nav-fill" role="tablist">
                 {
                     tools.map((tool, index) => (
-                        <button key={index} className={ 'nav-link ' + (index === 0 ? 'active' : '')}
-                                data-bs-toggle="tab" data-bs-target={ "#" + tool } role="tab">
+                        <button key={index} className={ index === 0 ? active : dormant } data-bs-toggle="tab" data-bs-target={ "#" + tool } >
                             { tool.charAt(0).toUpperCase() + tool.slice(1) }
                         </button>
                     ))
