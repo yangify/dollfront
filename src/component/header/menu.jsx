9249c65e4135372ad429f72175eadba5abc5380a
hongyang <hong.yang.code@gmail.com>
2021-05-20 09:18:27 +0800

    updated link of explore

--- src/component/header/menu.jsx
+++ src/component/header/menu.jsx
@@ -5,7 +5,8 @@ export const Menu = () => {
         <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
             <li><Link to="/" className="nav-link px-2 text-secondary">Home</Link></li>
             <li><Link to="/upload" className="nav-link px-2 text-white">Upload</Link></li>
-            <li><Link to="/explore" className="nav-link px-2 text-white">Explore</Link></li>
+            <li><a href={process.env.REACT_APP_SOURCE_GRAPH_URL} className="nav-link px-2 text-white">Explore</a></li>
+            <li><Link to="/configure" className="nav-link px-2 text-white">Configure</Link></li>
         </ul>
     );
 }
