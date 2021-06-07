150f8b45087871978ddf575125985e13842293ac
hongyang <hong.yang.code@gmail.com>
2021-06-07 12:42:44 +0800

    renamed explore to sourcegraph

--- src/component/header/menu.jsx
+++ src/component/header/menu.jsx
@@ -6,7 +6,7 @@ export const Menu = () => {
             <li><Link to="/" className="nav-link px-2 text-secondary">Home</Link></li>
             <li><Link to="/upload" className="nav-link px-2 text-white">Upload</Link></li>
             <li><Link to="/configure" className="nav-link px-2 text-white">Configure</Link></li>
-            <li><a href={process.env.REACT_APP_SOURCE_GRAPH_URL} className="nav-link px-2 text-white">Explore</a></li>
+            <li><a href={process.env.REACT_APP_SOURCE_GRAPH_URL} className="nav-link px-2 text-white">Sourcegraph</a></li>
         </ul>
     );
 }
