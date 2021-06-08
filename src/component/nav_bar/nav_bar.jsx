254a4df82b4aa7b40dd1a3cb407a38f90eff0da3
hongyang <hong.yang.code@gmail.com>
2021-06-08 15:34:35 +0800

    pin nav-bar to top

--- src/component/nav_bar/nav_bar.jsx
+++ src/component/nav_bar/nav_bar.jsx
@@ -3,9 +3,9 @@ import SubHeader from "./subheader/subheader";
 
 export const NavBar = () => {
     return (
-        <>
+        <div className="sticky-top">
             <Header />
             <SubHeader />
-        </>
+        </div>
     );
 };
