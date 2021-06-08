63a858f94b84e502100dc31b8ffd75e2c3e4a2c1
hongyang <hong.yang.code@gmail.com>
2021-06-08 11:13:14 +0800

    added subheader

--- src/component/header/header.jsx
+++ src/component/header/header.jsx
@@ -5,9 +5,9 @@ import { SignupButton } from "./signup_button";
 
 export const Header = () => {
     return (
-        <header className="p-3 bg-dark text-white">
-            <div className="container">
-                <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+        <>
+            <nav className="px-4 py-3 bg-dark text-white">
+                <div className="container-fluid d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
                     <Logo />
                     <Menu />
                     <div className="text-end">
@@ -15,7 +15,13 @@ export const Header = () => {
                         <SignupButton />
                     </div>
                 </div>
-            </div>
-        </header>
+            </nav>
+            <nav className="px-5 py-2 bg-light text-dark rounded shadow">
+                <div className="container-fluid d-flex justify-content-between text-center">
+                    <div className="h4 text-dark mt-2">Test</div>
+                    <button type="button" className="px-3 btn btn-warning shadow-none">Add</button>
+                </div>
+            </nav>
+        </>
     );
 }
