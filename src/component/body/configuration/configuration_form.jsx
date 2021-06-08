6efe17754a828cd242f9db60a293ec389585f6df
hongyang <hong.yang.code@gmail.com>
2021-06-08 11:32:28 +0800

    updated form as collapse

--- src/component/body/configuration/configuration_form.jsx
+++ src/component/body/configuration/configuration_form.jsx
@@ -1,7 +1,6 @@
 export const ConfigurationForm = () => {
     return (
-
-        <div className='card mb-4'>
+        <div className='card mb-4 collapse' id='configurationForm'>
             <div className='card-body'>
                 <form action='http://localhost:5000/api/configuration' method='post'>
                     <input type='text' name='name' /><br/>
--- src/component/nav_bar/subheader.jsx
+++ src/component/nav_bar/subheader.jsx
@@ -3,7 +3,8 @@ export const SubHeader = () => {
         <nav className="px-4 py-2 bg-light text-dark rounded shadow">
             <div className="container-fluid d-flex justify-content-between text-center">
                 <div className="h4 text-dark mt-2">Test</div>
-                <button type="button" className="px-3 btn btn-warning shadow-none">Add</button>
+                <button type="button" className="px-3 btn btn-warning shadow-none"
+                        data-bs-toggle="collapse" data-bs-target="#configurationForm">Add</button>
             </div>
         </nav>
     );
