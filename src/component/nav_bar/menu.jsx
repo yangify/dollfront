1161ac795fc68d70c9468b60e0cefb576fded1e1
hongyang <hong.yang.code@gmail.com>
2021-06-08 11:22:53 +0800

    updated styling for nav-bar

--- src/component/nav_bar/menu.jsx
+++ src/component/nav_bar/menu.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 export const Menu = () => {
     return (
-        <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+        <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0 ms-auto">
             <li><Link to="/" className="nav-link px-2 text-secondary">Home</Link></li>
             <li><Link to="/upload" className="nav-link px-2 text-white">Upload</Link></li>
             <li><Link to="/configure" className="nav-link px-2 text-white">Configure</Link></li>
