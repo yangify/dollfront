470f2f53f746b1cd36432cb03c183c941c9f620a
hongyang <hong.yang.code@gmail.com>
2021-06-15 16:46:47 +0800

    refactored form to take group input

--- src/component/body/configuration/configuration_form.jsx
+++ src/component/body/configuration/configuration_form.jsx
@@ -5,25 +5,53 @@ import { CONFIGURATION } from "../../../pages";
 
 export const ConfigurationForm = () => {
 
-    const [title, setTitle] = useState('');
-    const [query, setQuery] = useState('');
+    const [groupName, setGroupName] = useState(undefined);
+    const [inputs, setInputs] = useState([{title: undefined, query: undefined}])
 
-    const changeTitle = e => { setTitle(e.target.value) };
-    const changeQuery = e => { setQuery(e.target.value) };
-    const onClick = () => {
+    const updateGroupName = e => { setGroupName(e.target.value) }
+
+    const addInput = () => {
+        setInputs(prevState => [...prevState, {title: undefined, query: undefined}]);
+    }
+
+    const updateInput = (variable, index) => async e => {
+        const newInputs = [...inputs];
+        newInputs[index][variable] = e.target.value;
+        setInputs(newInputs);
+    };
+
+    const deleteInput = (index) => {
+        const newInputs = inputs.filter((input, i) => i !== index);
+        setInputs(newInputs);
+    }
+
+    const submitInputs = () => {
         const url = process.env.REACT_APP_SERVER_URL + process.env.REACT_APP_CONFIGURATION_ENDPOINT;
-        const data = new FormData();
-        data.append('title', title);
-        data.append('query', query);
+        const data = {'group-name': groupName, 'data': inputs}
         axios.post(url, data).then(() => window.location.replace(CONFIGURATION.link))
     }
 
     return (
         <div className='card mb-4 collapse' id='configurationForm'>
+            <button onClick={addInput}>Add input</button>
             <div className='card-body'>
-                <input type='text' name='title' onChange={changeTitle} /><br/>
-                <textarea name='query' onChange={changeQuery} /><br/>
-                <input type='submit' onClick={onClick}/>
+                <input type='text' name='group-name' placeholder='Group name' onChange={updateGroupName}/>
+                {
+                    inputs.map((input, index) => (
+                        <div key={index} className='row mt-3'>
+                            <div className='col'>
+                                <input type='text' name='title' placeholder={input.title || 'Title'}
+                                       onChange={updateInput('title', index)} />
+                                <input type='text' name='query' placeholder={input.query || 'Query'}
+                                       onChange={updateInput('query', index)} />
+                                <button onClick={() => deleteInput(index)}>Delete</button>
+                            </div>
+                        </div>
+                    ))
+                }
+            </div>
+            <div className='card-footer'>
+                <input type='submit' onClick={submitInputs}/>
             </div>
         </div>
     );
