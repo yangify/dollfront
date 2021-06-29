8731355fb4a3be9e9968e56ada0d23021bbf7285
hongyang <hong.yang.code@gmail.com>
2021-06-29 10:05:53 +0800

    removed loading toggle

--- src/component/body/home/detection/detection_card.jsx
+++ src/component/body/home/detection/detection_card.jsx
@@ -3,23 +3,11 @@ import { connect } from "react-redux";
 
 import DetectionGroupTabs from "./tab/detection_group_tabs";
 import DetectionGroupContent from "./content/detection_group_content";
-import { getDetections, toggleLoadingState } from "../../../../redux/dispatcher/detection_dispatcher";
+import { getDetections } from "../../../../redux/dispatcher/detection_dispatcher";
 
-const DetectionCard = ({ detectionGroups, selected, getDetections, toggleLoadingState }) => {
+const DetectionCard = ({ detectionGroups, selected, getDetections }) => {
 
-    useEffect(() => {
-        toggleLoadingState(true);
-        getDetections(selected.name);
-        toggleLoadingState(false);
-    }, [getDetections, selected, toggleLoadingState])
-
-    if (detectionGroups.isLoading) {
-        return (
-            <div className="spinner-border" role="status">
-                <span className="visually-hidden">Loading...</span>
-            </div>
-        )
-    }
+    useEffect(() => { getDetections(selected.name); }, [getDetections, selected])
 
     return (
         <>
@@ -41,6 +29,6 @@ const mapStateToProps = state => ({
     selected: state.apks.selected,
 });
 
-const mapDispatchToProps = { getDetections, toggleLoadingState }
+const mapDispatchToProps = { getDetections }
 
 export default connect(mapStateToProps, mapDispatchToProps)(DetectionCard)
