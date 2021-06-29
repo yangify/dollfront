3bbfcb9a0e2fc055a5f95577dae657bf4ee5d148
hongyang <hong.yang.code@gmail.com>
2021-06-29 14:55:19 +0800

    updated radio button

--- src/component/body/configuration/form/configuration_form_input.jsx
+++ src/component/body/configuration/form/configuration_form_input.jsx
@@ -3,24 +3,26 @@ import { connect } from "react-redux";
 const ConfigurationFormInput = ({ index, input, updateInput, deleteInput }) => {
 
     return (
-            <div className='list-group-item py-4'>
+            <form className='list-group-item py-4'>
                 <input className="form-control mb-2" type='text' name='title'
-                       placeholder={input.title || 'Title'} onChange={updateInput('title', index)}/>
+                       placeholder={input.title || 'Title'} onChange={updateInput('title', index)} required/>
                 <input className="form-control mb-2" type='text' name='searchTerm'
-                       placeholder={input.query || 'Search Term'} onChange={updateInput('searchTerm', index)}/>
+                       placeholder={input.query || 'Search Term'} onChange={updateInput('searchTerm', index)} required/>
 
                 <div className="form-check form-check-inline">
-                    <input className="form-check-input" type="radio" value="literal" onChange={updateInput('patternType', index)} />
+                    <input className="form-check-input" type="radio" name="inlineRadioOptions" id="literal"
+                           value="literal" onChange={updateInput('patternType', index)} checked/>
                     <label className="form-check-label">Literal</label>
                 </div>
 
                 <div className="form-check form-check-inline">
-                    <input className="form-check-input" type="radio" value="regexp" onChange={updateInput('patternType', index)}/>
+                    <input className="form-check-input" type="radio" name="inlineRadioOptions" id="regexp"
+                           value="regexp" onChange={updateInput('patternType', index)}/>
                     <label className="form-check-label">Regular expression</label>
                 </div>
 
-                <button onClick={() => deleteInput(index)}>Delete</button>
-            </div>
+                <button type="submit" onClick={() => deleteInput(index)}>Delete</button>
+            </form>
     );
 }
 
