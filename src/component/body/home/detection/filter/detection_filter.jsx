3848f59dd7173cb27f0289e5fd64248ee9445f7b
hongyang <hong.yang.code@gmail.com>
2021-07-12 15:33:37 +0800

    added checkbox toggling

--- src/component/body/home/detection/filter/detection_filter.jsx
+++ src/component/body/home/detection/filter/detection_filter.jsx
@@ -1,18 +1,25 @@
 import { connect } from "react-redux";
+import {useState} from "react";
 
 const DetectionFilter = ({index, showFilter}) => {
 
     const displayFilter = showFilter[index];
+
+    const [filters, setState] = useState({ android: {isChecked: true}});
+
+    const onChange = key => {
+        filters[key].isChecked = !filters[key].isChecked;
+        setState({...filters});
+    };
 
     return (
         <>
             {
                 displayFilter ?
                     <div className="form-check mx-4 mt-4">
-                        <input className="form-check-input" type="checkbox" value="" id="flexCheckChecked" />
-                        <label className="form-check-label" htmlFor="flexCheckChecked">
-                            Android links
-                        </label>
+                        <input className="form-check-input checked" type="checkbox" value="" id="flexCheckChecked"
+                               checked={filters['android'].isChecked} onChange={() => onChange('android')}/>
+                        <label className="form-check-label" htmlFor="flexCheckChecked">Android links</label>
                     </div>:
                     <></>
             }
