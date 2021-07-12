5cb1ce6278b081569e002643e0a360d1df58e834
hongyang <hong.yang.code@gmail.com>
2021-07-12 15:33:45 +0800

    added function to filter links

--- src/component/body/home/detection/content/detection_group_content.jsx
+++ src/component/body/home/detection/content/detection_group_content.jsx
@@ -3,12 +3,18 @@ import { connect } from "react-redux";
 import DetectionList from "./detection_list";
 
 const DetectionGroupContent = ({ group }) => {
+
+    const filter = detections => {
+        const links = ["http://schemas.android.com/apk/res/android"];
+        return {...detections, data : detections.data.filter(detection => !links.includes(detection.link))};
+    }
+
     return (
         <div className="card-body overflow-auto">
             <div className="tab-content">
                 {
                     group.data.map((detections, index) => (
-                        <DetectionList key={index} detectionList={detections} active={index === 0}/>
+                        <DetectionList key={index} detectionList={filter(detections)} active={index === 0}/>
                     ))
                 }
             </div>
